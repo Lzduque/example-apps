47facafed6401734db75b1270af51e832360b526
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2022-11-15 09:50:15 -0500

    add buttons for signin view

--- todo/frontend/app.js
+++ todo/frontend/app.js
@@ -3,19 +3,15 @@ const signedIn = false
 const showSignInView = () => {
 	const app = document.getElementById('app')
 
-	const content = document.createElement('content')
-	content.setAttribute('id', 'content')
-	app.append(content)
-
 	// REGISTER
 	const register = document.createElement('h2')
 	register.setAttribute('id', 'register')
 	register.innerText = 'Register'
-	content.append(register)
+	app.append(register)
 
 	const registerDiv = document.createElement('div')
 	registerDiv.setAttribute('id', 'register-div')
-	content.append(registerDiv)
+	app.append(registerDiv)
 
 	const emailDiv1 = document.createElement('div')
 	emailDiv1.setAttribute('id', 'email-div1')
@@ -33,7 +29,7 @@ const showSignInView = () => {
 
 	const passwordDiv1 = document.createElement('div')
 	passwordDiv1.setAttribute('id', 'password-div1')
-	content.append(passwordDiv1)
+	registerDiv.append(passwordDiv1)
 
 	const passwordLabel1 = document.createElement('label')
 	passwordLabel1.setAttribute('id', 'password-label1')
@@ -45,15 +41,20 @@ const showSignInView = () => {
 	passwordField1.setAttribute('type', 'password')
 	passwordDiv1.append(passwordField1)
 
+	const submitButton1 = document.createElement('button')
+	submitButton1.setAttribute('id', 'submit-button')
+	submitButton1.innerText = 'Submit'
+	registerDiv.append(submitButton1)
+
 	// SIGN IN
 	const signin = document.createElement('h2')
 	signin.setAttribute('id', 'signin')
 	signin.innerText = 'Sign In'
-	content.append(signin)
+	app.append(signin)
 
 	const signinDiv = document.createElement('div')
 	signinDiv.setAttribute('id', 'signin-div')
-	content.append(signinDiv)
+	app.append(signinDiv)
 
 	const emailDiv2 = document.createElement('div')
 	emailDiv2.setAttribute('id', 'email-div2')
@@ -71,7 +72,7 @@ const showSignInView = () => {
 
 	const passwordDiv2 = document.createElement('div')
 	passwordDiv2.setAttribute('id', 'password-div2')
-	content.append(passwordDiv2)
+	signinDiv.append(passwordDiv2)
 
 	const passwordLabel2 = document.createElement('label')
 	passwordLabel2.setAttribute('id', 'password-label2')
@@ -82,9 +83,46 @@ const showSignInView = () => {
 	passwordField2.setAttribute('id', 'password-field2')
 	passwordField2.setAttribute('type', 'password')
 	passwordDiv2.append(passwordField2)
+
+	const submitButton2 = document.createElement('button')
+	submitButton2.setAttribute('id', 'submit-button')
+	submitButton2.innerText = 'Submit'
+	signinDiv.append(submitButton2)
 }
 
-const showListView = () => {}
+const showListView = () => {
+	const app = document.getElementById('app')
+
+	// Top Bar
+	const topBar = document.createElement('div')
+	topBar.setAttribute('id', 'topBar')
+	app.append(topBar)
+
+	const title = document.createElement('h2')
+	title.setAttribute('id', 'title')
+	title.innerText = 'Todo'
+	topBar.append(title)
+
+	const signOut = document.createElement('button')
+	signOut.setAttribute('id', 'signOut')
+	signOut.innerText = 'Sign Out'
+	topBar.append(signOut)
+
+	// Main
+	const main = document.createElement('div')
+	main.setAttribute('id', 'main')
+	app.append(main)
+
+	// Side Bar
+	const sideBar = document.createElement('div')
+	sideBar.setAttribute('id', 'sideBar')
+	main.append(sideBar)
+
+	// List View
+	const listView = document.createElement('div')
+	listView.setAttribute('id', 'listView')
+	main.append(listView)
+}
 
 window.onload = (event) => {
 	if (signedIn) {
