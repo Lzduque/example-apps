209d7f760d8df296e18313648de89808160dcb39
Leticia Zamolo Duque <43674302+Lzduque@users.noreply.github.com>
2022-11-15 10:58:17 -0500

    refactor signin view

--- todo/frontend/app.js
+++ todo/frontend/app.js
@@ -1,94 +1,90 @@
 // Mock for view
-const signedIn = true
+const signedIn = false
 
 const showSignInView = () => {
 	const app = document.getElementById('app')
 
 	// REGISTER
-	const register = document.createElement('h2')
-	register.setAttribute('id', 'register')
-	register.innerText = 'Register'
-	app.append(register)
+	const register = addElement(app, 'h2', 'register', [], 'Register')
 
-	const registerDiv = document.createElement('div')
-	registerDiv.setAttribute('id', 'register-div')
-	app.append(registerDiv)
+	const registerDiv = addElement(app, 'div', 'registerDiv')
 
-	const emailDiv1 = document.createElement('div')
-	emailDiv1.setAttribute('id', 'email-div1')
-	registerDiv.append(emailDiv1)
+	const emailDiv1 = addElement(registerDiv, 'div', 'emailDiv1')
 
-	const emailLabel1 = document.createElement('label')
-	emailLabel1.setAttribute('id', 'email-label1')
-	emailLabel1.innerText = 'Email'
-	emailDiv1.append(emailLabel1)
+	const emailLabel1 = addElement(
+		emailDiv1,
+		'label',
+		'emailLabel1',
+		[],
+		'Email'
+	)
 
-	const emailField1 = document.createElement('input')
-	emailField1.setAttribute('id', 'email-field1')
-	emailField1.setAttribute('type', 'email')
-	emailDiv1.append(emailField1)
+	const emailField1 = addElement(emailDiv1, 'input', 'emailField1', [
+		['type', 'email'],
+	])
 
-	const passwordDiv1 = document.createElement('div')
-	passwordDiv1.setAttribute('id', 'password-div1')
-	registerDiv.append(passwordDiv1)
+	const passwordDiv1 = addElement(registerDiv, 'div', 'passwordDiv1')
 
-	const passwordLabel1 = document.createElement('label')
-	passwordLabel1.setAttribute('id', 'password-label1')
-	passwordLabel1.innerText = 'Password'
-	passwordDiv1.append(passwordLabel1)
+	const passwordLabel1 = addElement(
+		passwordDiv1,
+		'label',
+		'passwordLabel1',
+		[],
+		'Password'
+	)
 
-	const passwordField1 = document.createElement('input')
-	passwordField1.setAttribute('id', 'password-field1')
-	passwordField1.setAttribute('type', 'password')
-	passwordDiv1.append(passwordField1)
+	const passwordField1 = addElement(passwordDiv1, 'input', 'passwordField1', [
+		['type', 'password'],
+	])
 
-	const submitButton1 = document.createElement('button')
-	submitButton1.setAttribute('id', 'submit-button')
-	submitButton1.innerText = 'Submit'
-	registerDiv.append(submitButton1)
+	const submitButton1 = addElement(
+		registerDiv,
+		'button',
+		'submitButton1',
+		[],
+		'Submit'
+	)
 
 	// SIGN IN
-	const signin = document.createElement('h2')
-	signin.setAttribute('id', 'signin')
-	signin.innerText = 'Sign In'
-	app.append(signin)
+	const signin = addElement(app, 'h2', 'signin', [], 'Sign In')
 
-	const signinDiv = document.createElement('div')
-	signinDiv.setAttribute('id', 'signin-div')
-	app.append(signinDiv)
+	const signinDiv = addElement(app, 'div', 'signinDiv')
 
-	const emailDiv2 = document.createElement('div')
-	emailDiv2.setAttribute('id', 'email-div2')
-	signinDiv.append(emailDiv2)
+	const emailDiv2 = addElement(signinDiv, 'div', 'emailDiv2')
 
-	const emailLabel2 = document.createElement('label')
-	emailLabel2.setAttribute('id', 'email-label2')
-	emailLabel2.innerText = 'Email'
-	emailDiv2.append(emailLabel2)
+	const emailLabel2 = addElement(
+		emailDiv2,
+		'label',
+		'emailLabel2',
+		[],
+		'Email'
+	)
 
-	const emailField2 = document.createElement('input')
-	emailField2.setAttribute('id', 'email-field2')
-	emailField2.setAttribute('type', 'email')
-	emailDiv2.append(emailField2)
+	const emailField2 = addElement(emailDiv2, 'input', 'emailField2', [
+		['type', 'email'],
+	])
 
-	const passwordDiv2 = document.createElement('div')
-	passwordDiv2.setAttribute('id', 'password-div2')
-	signinDiv.append(passwordDiv2)
+	const passwordDiv2 = addElement(signinDiv, 'div', 'passwordDiv2')
 
-	const passwordLabel2 = document.createElement('label')
-	passwordLabel2.setAttribute('id', 'password-label2')
-	passwordLabel2.innerText = 'Password'
-	passwordDiv2.append(passwordLabel2)
+	const passwordLabel2 = addElement(
+		passwordDiv2,
+		'label',
+		'passwordLabel2',
+		[],
+		'Password'
+	)
 
-	const passwordField2 = document.createElement('input')
-	passwordField2.setAttribute('id', 'password-field2')
-	passwordField2.setAttribute('type', 'password')
-	passwordDiv2.append(passwordField2)
+	const passwordField2 = addElement(passwordDiv2, 'input', 'passwordField2', [
+		['type', 'password'],
+	])
 
-	const submitButton2 = document.createElement('button')
-	submitButton2.setAttribute('id', 'submit-button')
-	submitButton2.innerText = 'Submit'
-	signinDiv.append(submitButton2)
+	const submitButton2 = addElement(
+		signinDiv,
+		'button',
+		'submitButton2',
+		[],
+		'Submit'
+	)
 }
 
 const showListView = () => {
